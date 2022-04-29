958c79185738176f3fbdab45e3ef53f072a7d120
Juan Bocanegra <jbocane6@gmail.com>
2022-04-28 22:12:20 -0500

    [UPDATE] file 3 v.2

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -7,6 +7,6 @@
 --     Your script can be executed on any database
 
 
-SELECT band_name, COALESCE(2022) - formed as lifespan
+SELECT band_name, COALESCE(split, 2020) - formed as lifespan
 FROM metal_bands
 WHERE style like "%Glam rock%";
