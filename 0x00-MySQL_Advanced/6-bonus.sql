e3e6cb645476956e866c75d42ed5d641460553af
Juan Bocanegra <jbocane6@gmail.com>
2022-04-28 23:10:27 -0500

    6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -18,10 +18,9 @@ CREATE PROCEDURE AddBonus(
 )
 BEGIN
     INSERT INTO projects (name)
-    SELECT project_name from TEMP
+    SELECT project_name from DUAL
     WHERE NOT EXISTS (SELECT * FROM projects WHERE name=project_name LIMIT 1);
     INSERT INTO corrections (user_id, project_id, score)
     VALUES(user_id, (SELECT id from projects WHERE name=project_name), score);
-END
-$$
+END $$
 DELIMITER ;
